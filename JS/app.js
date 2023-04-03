e34b89a452b5c23da2d83c861e3bdfec3d6650a7
MhamadBelal <blalabomalek2000@gmail.com>
2023-04-03 20:51:54 +0000

    adding pop boxes in javascript

---
+++ JS/app.js
@@ -0,0 +1,21 @@
+let name1 = prompt("Enter your Name");
+
+
+let gender = prompt("enter your gender: male/female");
+
+if (gender != "male" || gender != "female") {
+    gender= prompt("enter your gender: male/female");
+}
+let age = prompt("enter you age")
+if (age <= 0)
+    alert("age is less than or equal to zero");
+
+let ms = confirm("whould you like to see the welcoming message?")
+if (ms) {
+    if (gender == "male")
+        alert("welcome to our page Mr " + name1);
+    else if (gender == "female")
+        alert("welcome to our page Ms " + name1);
+    else
+        alert("welcome to our page");
+}
