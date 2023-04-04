a1b56573ad8cfd82a28603ba0c2c0a25ec029077
MhamadBelal <blalabomalek2000@gmail.com>
2023-04-04 13:08:23 +0000

    age meassage

--- JS/app.js
+++ JS/app.js
@@ -4,15 +4,14 @@ let name1 = prompt("Enter your Name");
 let gender = prompt("enter your gender: male/female");
 
 if (gender == "male" || gender == "female") {
-    
+
 }
-else
-{
-    gender= prompt("enter your gender: male/female");
+else {
+    gender = prompt("enter your gender: male/female");
 }
 let age = prompt("enter you age")
 if (age <= 0)
-    age = prompt("age is less than or equal to zero");
+    age = prompt("age is less than or equal to zero, please enter you age agian");
 
 let ms = confirm("whould you like to see the welcoming message?")
 if (ms) {
